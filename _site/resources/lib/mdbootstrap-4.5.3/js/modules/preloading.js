04e33e2ee1704c85462142c3a27b4cd5d32c940c
Multinerd <multinerd@users.noreply.github.com>
2018-06-04 14:30:30 -0400

    Update Sites

--- _site/resources/lib/mdbootstrap-4.5.3/js/modules/preloading.js
+++ _site/resources/lib/mdbootstrap-4.5.3/js/modules/preloading.js
@@ -3,7 +3,7 @@
 // Preloading script
 
 $(document).ready(function () {
-  $('#preloader-markup').load('mdb-addons/preloader.html', function () {
+  $('#preloader-markup').load('/resources/lib/mdbootstrap-4.5.3/mdb-addons/preloader.html', function () {
     $(window).on('load', function () {
       $('#mdb-preloader').fadeOut('slow');
     });
